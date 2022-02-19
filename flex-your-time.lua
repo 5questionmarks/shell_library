6e33980673f634b5370cb8d6cf364e77612ef0cd
5questionmarks <79291691+5questionmarks@users.noreply.github.com>
2022-02-18 16:47:52 -0800

    Update flex-your-time.lua

--- flex-your-time.lua
+++ flex-your-time.lua
@@ -1,107 +1,16 @@
 local Shell = loadstring(game:HttpGet("https://raw.githubusercontent.com/5questionmarks/shell_library/main/base.lua"))()
 
-local Shell_LIB = Shell:CreateLib("Flex Your Time")
---^ LIB Setup
-local PlayerPage = Shell_LIB:CreateTab("Player")
---^ Player Page
-PlayerPage:CreateButton("Fly - LeftAlt",function()
-    local player = game.Players.LocalPlayer
-	local char = player.Character or player.CharacterAdded:Wait()
+local Shell_LIB = Shell:CreateLib("Steal Others Time")
 
-	local cam = workspace.CurrentCamera
-	local uis = game:GetService("UserInputService")
+local SwordPage = Shell_LIB:CreateTab("Sword",3)
 
-	local idleAnim ;
-	local forwardAnim;
+local Send = print
 
+local MiscPage = Shell_LIB:CreateTab("Misc",3)
 
-	local w = false
-	local s = false
-	local a = false
-	local d = false
+local PlayerPage = Shell_LIB:CreateTab("Player",3)
 
-	local flying = false
-	uis.InputBegan:Connect(function(key, chat)
-		if key.KeyCode == Enum.KeyCode.LeftAlt then
-			if flying then --Stop Flying
-				flying = false
-				char.Animate.Disabled = false
-
-			else --Start Flying
-				flying = true
-
-				char.Animate.Disabled = true
-
-				local bodyvel = Instance.new("BodyVelocity", char.PrimaryPart)
-				bodyvel.MaxForce = Vector3.new(math.huge,math.huge,math.huge)
-				bodyvel.Velocity = Vector3.new(0,0,0)
-				bodyvel.Name = "fly"
-
-				repeat wait(0.1) until flying == false
-				bodyvel:Destroy()
-				end
-		end
-
-		if key.KeyCode == Enum.KeyCode.W then
-			w = true
-		elseif key.KeyCode == Enum.KeyCode.S then
-			s = true
-		elseif key.KeyCode == Enum.KeyCode.A then
-			a = true
-		elseif key.KeyCode == Enum.KeyCode.D then
-			d = true
-		end
-	end)
-
-	uis.InputEnded:Connect(function(key)	
-		if key.KeyCode == Enum.KeyCode.W then
-			w = false
-		elseif key.KeyCode == Enum.KeyCode.S then
-			s = false
-		elseif key.KeyCode == Enum.KeyCode.A then
-			a = false
-		elseif key.KeyCode == Enum.KeyCode.D then
-			d = false
-		end
-	end)
-
-	while wait() do
-		if flying then
-			char.PrimaryPart:FindFirstChild("fly").Velocity = Vector3.new(0,0,0)
-			
-			if w then
-				char.PrimaryPart:FindFirstChild("fly").Velocity = cam.CFrame.LookVector * 50
-			end
-			if s then
-				char.PrimaryPart:FindFirstChild("fly").Velocity = cam.CFrame.LookVector * -50
-			end
-			if a then
-				char.PrimaryPart:FindFirstChild("fly").Velocity = cam.CFrame.RightVector * -50
-			end
-			if d then
-				char.PrimaryPart:FindFirstChild("fly").Velocity = cam.CFrame.RightVector * 50
-			end
-		else
-			wait()
-		end
-	end
-end)
-
-PlayerPage:CreateSlider("WalkSpeed",55,16,function(s)
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
-end)
-
-PlayerPage:CreateButton("Rejoin",function()
-     local service = game:GetService("TeleportService")
-     local player = game.Players.LocalPlayer
-     local PlaceID = "5278850819"
-     service:Teleport(PlaceID, player)
-end)
-
-local SwordPage = Shell_LIB:CreateTab("Sword")
---^ SwordPage
-
-SwordPage:CreateSlider("Sword Reach",20,10,function(reachsize)
+SwordPage:CreateSlider("Sword Reach",16,5,function(reachsize)
     local plr = game.Players.LocalPlayer
     plr.Character:WaitForChild("Humanoid"):UnequipTools()
         local tool = plr.Backpack:FindFirstChildOfClass("Tool")
@@ -118,64 +27,101 @@ SwordPage:CreateSlider("Sword Reach",20,10,function(reachsize)
     tool:Activate()
 end)
 
-local ChatPage = Shell_LIB:CreateTab("Chat")
-
-ChatPage:CreateDropDown("Messages",{"Shell Shock on top!","Not hacks, just Gaming Chair.","Hacks? What's that? I'm new.."},function(arg)
-    if arg == "Shell Shock on top!" then
-        local Message = "Shell Shock on top!"
-        local All = "All"
-        local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
-        Event:FireServer(Message, All)
-    end
-    if arg == "Not hacks, just Gaming Chair." then
-        local Message = "Not hacks, just Gaming Chair."
-        local All = "All"
-        local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
-        Event:FireServer(Message, All)
-    end
-    if arg == "Hacks? What's that? I'm new.." then
-        local Message = "Hacks? What's that? I'm new.."
-        local All = "All"
-        local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
-        Event:FireServer(Message, All)
-    end
+MiscPage:CreateButton("Rejoin",function()
+  local service = game:GetService("TeleportService")
+  local player = game.Players.LocalPlayer
+  local PlaceID = "5278850819"
+  service:Teleport(PlaceID, player)
 end)
 
---^ Bypass
-function AntiBan()
+MiscPage:CreateButton("ESP",function()
+local ESP = Instance.new("BillboardGui")
+local player = Instance.new("TextLabel",ESP) 
+
+ESP.Name = "ESP"; 
+ESP.ResetOnSpawn = false
+ESP.AlwaysOnTop = true;
+ESP.LightInfluence = 0;
+ESP.Size = UDim2.new(1.75, 0, 1.75, 0);
+player.BackgroundColor3 = Color3.fromRGB(255, 255, 255);
+player.BackgroundTransparency = 1 
+player.Text = ""
+player.Size = UDim2.new(0.0001, 0.00001, 0.0001, 0.00001);
+player.BorderSizePixel = 4;
+player.BorderColor3 = Color3.new(255, 255, 255)
+player.BorderSizePixel = 0
+player.Font = "GothamSemibold"
+player.TextSize = 13
+player.TextColor3 = Color3.fromRGB(255, 255, 255)
+
+game:GetService("RunService").RenderStepped:Connect(function() 
+	for i,v in pairs (game:GetService("Players"):GetPlayers()) do
+		if v ~= game:GetService("Players").LocalPlayer and v.Character.Head:FindFirstChild("ESP") == nil then 
+			player.Text = "Player: "..v.Name
+			ESP:Clone().Parent = v.Character.Head
+		end
+	end
+end)
+end)
+
+MiscPage:CreateButton("Create Block",function()
+    local Part = Instance.new("Part")
+    Part.Parent = game.Workspace
+    Part.Color = Color3.fromRGB(255,255,255)
+    Part.Anchored = true
+    Part.Size = Vector3.new(1000,0.1,1000)
+    Part.Position = Vector3.new(-0.223445, 4.99795, -0.338424)
+    Part.Transparency = 0.9
+    local Send = print
+
+    print('block: ' .. tostring(game.Players.LocalPlayer))
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = 0
+    game.Players.LocalPlayer.Character.Humanoid.Torso.CFrame = CFrame.new(-1.28283, 8.04746, -1.20013)
+end)
+
+PlayerPage:CreateSlider("WalkSpeed",54,16,function(s)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end) 
+
+function Bypass()
+    local Send = print
+    
+    Send('bypass: ' .. tostring(game.Players.LocalPlayer))
+    local Controller = game.Players.LocalPlayer.PlayerScripts.Client.Controller
+    local Client = game.Players.LocalPlayer.PlayerScripts.Client
+    local ControlModule = game.Players.LocalPlayer.PlayerScripts.PlayerModule.ControlModule
+    local CameraModule = game.Players.LocalPlayer.PlayerScripts.PlayerModule.CameraModule
+    local PlayerScripts = game.Players.LocalPlayer.PlayerScripts
+    Controller.Parent = PlayerScripts
+    CameraModule.Parent = PlayerScripts
+    ControlModule.Parent = PlayerScripts
+    for i,v in pairs (getconnections(Client.Changed)) do
+    v:Disable()
+    end
+    
     local GiftsFolder = game:GetService("Workspace").Gifts
     local MainAc = game.Players.LocalPlayer.Character.Anticheat
     local StarterPlayerAC = game.StarterPlayer.StarterCharacterScripts.Anticheat
 
-    function ChangeMainAcPath()
-        MainAc.Parent = GiftsFolder
-    end
-
-    function RemoveStarterPlayerAC()
-        StarterPlayerAC:Disable()
-        wait(1)
-        StarterPlayerAC:Destroy()
-    end
-
-    game:GetService("RunService").RenderStepped:Connect(function() 
-        ChangeMainAcPath()
-        RemoveStarterPlayerAC()
-    end)
-    wait(1)
-    print('Disabled StarterPlayer AntiCheat, Moved Main AntiCheat Path')
-    wait()
-    
-    local NewAntiCheatPath = game:GetService("Workspace").Gifts.Anticheat
-    NewAntiCheatPath:Destroy()
-    print('Removed Main AntiCheat')
-    while true do wait()
+function ChangeMainAcPath()
+    MainAc.Parent = GiftsFolder
+end
+function RemoveStarterPlayerAC()
+    StarterPlayerAC:Destroy()
+end
+ChangeMainAcPath()
+RemoveStarterPlayerAC()
+wait(1)
+local NewAntiCheatPath = game:GetService("Workspace").Gifts.Anticheat
+NewAntiCheatPath:Destroy()
+while true do wait()
     if game.Players.LocalPlayer.Character.Humanoid.Health == 0 then
-            local service = game:GetService("TeleportService")
-            local player = game.Players.LocalPlayer
-            local PlaceID = "5278850819"
-            service:Teleport(PlaceID, player)
-        end
+         local service = game:GetService("TeleportService")
+         local player = game.Players.LocalPlayer
+         local PlaceID = "5278850819"
+         service:Teleport(PlaceID, player)
     end
 end
+end
 
-AntiBan()
+Bypass()
